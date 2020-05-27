e823d47bb908837aca92fa3890a1817dcad8d08e
Isaac Schemm <isaacschemm@gmail.com>
2020-05-26 22:09:47 -0500

    Update Zeroconf

--- merge.bat
+++ merge.bat
@@ -1,4 +1,3 @@
 CD CastHelper\bin\Release
 MKDIR Merged
-"%USERPROFILE%\.nuget\packages\ilmerge\3.0.40\tools\net452\ILMerge.exe" /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2" /out:Merged\casthelper.exe casthelper.exe RokuDotNet.Client.dll System.Reactive.Core.dll System.Reactive.Interfaces.dll System.Reactive.Linq.dll System.Reactive.PlatformServices.dll System.Reactive.Windows.Threading.dll Zeroconf.dll
-@PAUSE
+"%USERPROFILE%\.nuget\packages\ilmerge\3.0.40\tools\net452\ILMerge.exe" /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2" /out:Merged\casthelper.exe casthelper.exe RokuDotNet.Client.dll System.Reactive.dll System.Runtime.CompilerServices.Unsafe.dll System.Threading.Tasks.Extensions.dll System.ValueTuple.dll Zeroconf.dll
