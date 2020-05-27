dc367c5455269008f080e48347600c18a7be63aa
Isaac Schemm <isaacschemm@gmail.com>
2020-05-26 21:49:20 -0500

    Fix ILMerge

--- merge.bat
+++ merge.bat
@@ -1,4 +1,4 @@
 CD CastHelper\bin\Release
 MKDIR Merged
-"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7" /out:Merged\casthelper.exe casthelper.exe System.Reactive.Core.dll System.Reactive.Interfaces.dll System.Reactive.Linq.dll System.Reactive.PlatformServices.dll System.Reactive.Windows.Threading.dll Zeroconf.dll
+"%USERPROFILE%\.nuget\packages\ilmerge\3.0.40\tools\net452\ILMerge.exe" /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2" /out:Merged\casthelper.exe casthelper.exe RokuDotNet.Client.dll System.Reactive.Core.dll System.Reactive.Interfaces.dll System.Reactive.Linq.dll System.Reactive.PlatformServices.dll System.Reactive.Windows.Threading.dll Zeroconf.dll
 @PAUSE
