fbd5b439f1900dc86dd2da51ed3d9099df9670e6
Isaac Schemm <isaacschemm@gmail.com>
2020-05-30 10:55:41 -0500

    Use submodules instead of NuGet to avoid dependency on System.Reactive

---
+++ Disposable.cs
@@ -0,0 +1,15 @@
+﻿namespace System.Reactive.Linq { }
+
+namespace System.Reactive.Disposables {
+	public static class Disposable {
+		private sealed class EmptyDisposable : IDisposable {
+			private EmptyDisposable() { }
+
+			public void Dispose() { }
+
+			public static readonly EmptyDisposable Singleton = new EmptyDisposable();
+		}
+
+		public static IDisposable Empty => EmptyDisposable.Singleton;
+	}
+}
--- merge.bat
+++
@@ -1,4 +0,0 @@
-CD CastHelper\bin\Release
-MKDIR Merged
-"%USERPROFILE%\.nuget\packages\ilmerge\3.0.40\tools\net452\ILMerge.exe" /targetplatform:"v4,C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.7.2" /out:Merged\casthelper.exe casthelper.exe RokuDotNet.Client.dll System.Reactive.dll System.Runtime.CompilerServices.Unsafe.dll System.Threading.Tasks.Extensions.dll System.ValueTuple.dll Zeroconf.dll
-COPY LICENSES.txt Merged
