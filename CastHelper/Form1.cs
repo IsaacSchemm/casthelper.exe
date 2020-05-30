a9085c933ce1a71d0e586d209c4a052fe7ba5404
Isaac Schemm <isaacschemm@gmail.com>
2020-05-30 10:17:27 -0500

    Create LICENSES.txt

--- CastHelper/Form1.cs
+++ CastHelper/Form1.cs
@@ -170,13 +170,7 @@ namespace CastHelper {
 Copyright © 2018-2020 Isaac Schemm
 https://github.com/IsaacSchemm/casthelper.exe
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the ""Software""), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-Zeroconf and System.Reactive, which are included for Apple TV discovery, are available under the Microsoft Public License and the Apache License 2.0, respectively.", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+See LICENSES.txt for license information.", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
 		}
 
 		/// <summary>
